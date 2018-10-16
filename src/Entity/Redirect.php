fefb27b3e5b691bb7eb85ddb83021e98d18cefa0
deviantintegral <deviantintegral@71291.no-reply.drupal.org>
2018-10-16 11:13:30 +0200

    Issue #2995075 by deviantintegral: Unicode methods are deprecated in Drupal 8.6.0

--- src/Entity/Redirect.php
+++ src/Entity/Redirect.php
@@ -3,7 +3,6 @@
 namespace Drupal\redirect\Entity;
 
 use Drupal\Component\Utility\Crypt;
-use Drupal\Component\Utility\Unicode;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\EntityStorageInterface;
@@ -62,7 +61,7 @@ class Redirect extends ContentEntityBase {
    */
   public static function generateHash($source_path, array $source_query, $language) {
     $hash = array(
-      'source' => Unicode::strtolower($source_path),
+      'source' => mb_strtolower($source_path),
       'language' => $language,
     );
 
