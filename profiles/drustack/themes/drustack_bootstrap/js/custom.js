e3e5d8021428f62c63318601b08b5614dfd7524b
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2023-07-17 22:57:02 +0800

    Update profiles/drustack/themes/drustack_bootstrap

--- profiles/drustack/themes/drustack_bootstrap/js/custom.js
+++ profiles/drustack/themes/drustack_bootstrap/js/custom.js
@@ -3,16 +3,16 @@
  * Global utilities.
  *
  */
-(function($, Drupal) {
+(function ($, Drupal) {
 
   'use strict';
 
   Drupal.behaviors.drustack_bootstrap = {
-    attach: function(context, settings) {
-
+    attach: function (context, settings) {
       // Custom code here
-
+      
     }
   };
 
 })(jQuery, Drupal);
+
